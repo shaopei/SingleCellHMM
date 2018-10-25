e8cd84702ca1fe6ff65010807e504b2d66380227
shaopei <sc2457@cornell.edu>
2018-10-25 17:55:03 -0400

    speed up

--- SingleCellHMM.bash
+++ SingleCellHMM.bash
@@ -1,6 +1,6 @@
-# bash SingleCellHMM.bash  05-007B1_gene_exon_tagged.REF_chr20.bam
+# bash SingleCellHMM.bash  pbmc4k_possorted_genome_bam.bam
 
-INPUT_BAM=$1 #05-007B1_gene_exon_tagged.REF_chr22.bam
+INPUT_BAM=$1 #pbmc4k_possorted_genome_bam.bam
 PREFIX=`echo ${INPUT_BAM} | rev | cut -d . -f 2- |rev`
 TMPDIR=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 mkdir ${TMPDIR}
@@ -13,38 +13,47 @@ echo "temp folder               $TMPDIR"
 echo ""
 echo "Reads spanning over splicing junction will join HMM blocks"
 echo "To avoid that, split reads into small blocks before input to groHMM"
-echo "Spliting and sorting reads..."
-bedtools bamtobed -i ${INPUT_BAM} -split |LC_ALL=C sort -k1,1V -k2,2n --parallel=30 |gzip > ${PREFIX}_split.bed.gz
+echo "Spliting reads..."
+bedtools bamtobed -i ${INPUT_BAM} -split |gzip > ${PREFIX}_split.bed.gz
 
-
+echo ""
+echo "Sorting reads in the background..."
+zcat ${PREFIX}_split.bed.gz |LC_ALL=C sort -k1,1V -k2,2n --parallel=30 | gzip > ${PREFIX}_split.sorted.bed.gz &
+ 
+echo ""
 echo "Start to run groHMM..."
 R --vanilla --slave --args $(pwd) ${PREFIX}_split.bed.gz < SingleCellHMM.R 
-gzip ${PREFIX}_split_HMM.bed
 
 
 
 
+echo ""
 echo "Merging HMM blocks within 500bp..."
 f=${PREFIX}_split_HMM
-zcat $f.bed.gz | grep + > ${f}_plus
-zcat $f.bed.gz | grep - > ${f}_minus
+cat $f.bed | grep + > ${f}_plus
+cat $f.bed | grep - > ${f}_minus
 bedtools merge -s -d 500 -i ${f}_plus > ${f}_plus_merge500
 bedtools merge -s -d 500 -i ${f}_minus > ${f}_minus_merge500
 rm ${f}_plus ${f}_minus
+gzip ${PREFIX}_split_HMM.bed
 
 cat ${f}_plus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "+"}' > ${f}_merge500
 cat ${f}_minus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "-"}' >> ${f}_merge500
 rm ${f}_plus_merge500 ${f}_minus_merge500
 
+echo ""
 echo "Calculating the coverage..." 
 LC_ALL=C sort -k1,1V -k2,2n ${f}_merge500 --parallel=30 > ${f}_merge500.sorted.bed
 rm ${f}_merge500
 
-bedtools coverage -a ${f}_merge500.sorted.bed -b ${PREFIX}_split.bed.gz -s -counts -split -sorted > ${f}_merge500.sorted.bed_count
+wait
+bedtools coverage -a ${f}_merge500.sorted.bed -b ${PREFIX}_split.sorted.bed.gz -s -counts -split -sorted > ${f}_merge500.sorted.bed_count
 
+echo ""
 echo "Filtering the HMM blocks by coverage..." 
 cat ${f}_merge500.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= 2){print $1, $2, $3, $4, $5, $6}' | gzip > ${f}_merge500_2reads.bed.gz
 cat ${f}_merge500.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= 5){print $1, $2, $3, $4, $5, $6}' | gzip > ${f}_merge500_5reads.bed.gz
 
+rm ${PREFIX}_split.bed.gz
 mv ${f}_merge500.sorted.bed ${f}_merge500.sorted.bed_count ${TMPDIR}/.
 gzip ${TMPDIR}/*
