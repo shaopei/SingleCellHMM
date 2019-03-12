237b40dce0ad3516446d46b3385c55f91c68f92c
shaopei <sc2457@cornell.edu>
2019-03-12 13:34:21 -0400

    work in temp

--- SingleCellHMM.bash
+++ SingleCellHMM.bash
@@ -1,10 +1,10 @@
 # bash SingleCellHMM.bash  Path_to_bam_file pbmc4k_possorted_genome_bam.bam 
-# make sure you can call SingleCellHMM.R in the current folder
 
 WD=$1
 INPUT_BAM=$2 #pbmc4k_possorted_genome_bam.bam
-cd ${WD}
-ln -s /workdir/sc2457/SingleCellHMM/SingleCellHMM.R .
+PL=/workdir/sc2457/SingleCellHMM/
+
+#cd ${WD}
 PREFIX=`echo ${INPUT_BAM} | rev | cut -d . -f 2- |rev`
 tmp=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 TMPDIR=${PREFIX}_${tmp}
@@ -12,7 +12,7 @@ mkdir ${TMPDIR}
 
 exec > >(tee SingleCellHMM_Run_${TMPDIR}.log)
 exec 2>&1
-
+echo "Path to SingleCellHMM.R   $PL" 
 echo "Path to INPUT_BAM         $WD"   
 echo "INPUT_BAM                 $INPUT_BAM"
 echo "temp folder               $TMPDIR"
@@ -20,23 +20,23 @@ echo ""
 echo "Reads spanning over splicing junction will join HMM blocks"
 echo "To avoid that, split reads into small blocks before input to groHMM"
 echo "Spliting and sorting reads..."
-bedtools bamtobed -i ${INPUT_BAM} -split |LC_ALL=C sort -k1,1V -k2,2n --parallel=30 | gzip > ${PREFIX}_split.sorted.bed.gz 
+bedtools bamtobed -i ${WD}/${INPUT_BAM} -split |LC_ALL=C sort -k1,1V -k2,2n --parallel=30 | gzip > ${TMPDIR}/${PREFIX}_split.sorted.bed.gz 
  
 echo ""
 echo "Start to run groHMM..."
-R --vanilla --slave --args $(pwd) ${PREFIX}_split.sorted.bed.gz  < SingleCellHMM.R 
+cd ${TMPDIR}
+R --vanilla --slave --args $(pwd) ${PREFIX}_split.sorted.bed.gz  < ${PL}/SingleCellHMM.R 
 
 
 echo ""
 echo "Merging HMM blocks within 500bp..."
-cd ${TMPDIR}
 f=${PREFIX}_split.sorted_HMM
-cat ${WD}/$f.bed | grep + > ${f}_plus
-cat ${WD}/$f.bed | grep - > ${f}_minus
+cat $f.bed | grep + > ${f}_plus
+cat $f.bed | grep - > ${f}_minus
 bedtools merge -s -d 500 -i ${f}_plus > ${f}_plus_merge500
 bedtools merge -s -d 500 -i ${f}_minus > ${f}_minus_merge500
 rm ${f}_plus ${f}_minus
-gzip ${WD}/${f}.bed &
+gzip ${f}.bed &
 
 cat ${f}_plus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "+"}' > ${f}_merge500
 cat ${f}_minus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "-"}' >> ${f}_merge500
